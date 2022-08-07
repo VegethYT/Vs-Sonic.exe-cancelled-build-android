8e42a8c598f1959162b5218f47801ba218b92d2e
Aly-Ant <90076069+Aly-Ant0@users.noreply.github.com>
2022-08-07 19:37:04 -0300

    update FreeplayItem.hx

--- source/FreeplayItem.hx
+++ source/FreeplayItem.hx
@@ -1,1 +1,27 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.graphics.frames.FlxAtlasFrames;
+import flixel.math.FlxMath;
+import flixel.util.FlxColor;
+
+class FreeplayItem extends FlxSprite
+{
+	public var targetY:Float = 0;
+
+	public function new(x:Float, y:Float, name:String = '')
+	{
+		super(x, y);
+		loadGraphic(Paths.image(name));
+		//trace('Test added: ' + WeekData.getWeekNumber(weekNum) + ' (' + weekNum + ')');
+		antialiasing = ClientPrefs.globalAntialiasing;
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+		y = FlxMath.lerp(y, (targetY * 120) + 480, CoolUtil.boundTo(elapsed * 10.2, 0, 1));
+	}
+}
 
