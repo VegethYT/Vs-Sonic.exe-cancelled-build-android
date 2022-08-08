9bd29bedeb1bf0f8a2ae4c78fad1d5a77d012c86
Aly-Ant <90076069+Aly-Ant0@users.noreply.github.com>
2022-08-07 23:44:39 -0300

    update FreeplayState.hx

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -110,6 +110,8 @@ class FreeplayState extends MusicBeatState
 
 				var char:FreeplayItem = new FreeplayItem(-20, 200, 'fpstuff/' + boxArray[i]);
 				char.ID = i;
+				char.y += ((char.width - 450) + 40);
+				char.setGraphicSize(Std.int(char.width * 0.5));
 				grpSongs.add(char);
 		}
 			//changeSelection();
