c7e1256793ff89e266ac7e4ddee581585251cddc
Aly-Ant <90076069+Aly-Ant0@users.noreply.github.com>
2022-08-08 04:24:32 -0300

    change

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -60,20 +60,20 @@ class FreeplayState extends MusicBeatState
 	public static var boxslct:Bool = true;
 	public static var songslct:Bool = false;
 	var cdman:Bool = true;
-
+		
 	var bg:FlxSprite;
 	var sideBars:FlxBackdrop;
-
+		
 	override function create()
 	{
 		Paths.clearStoredMemory();
 		Paths.clearUnusedMemory();
-
+		
 		persistentUpdate = true;
 		PlayState.isStoryMode = false;
-
+		
 		/*		//KIND OF BROKEN NOW AND ALSO PRETTY USELESS//
-
+		
 		var initSonglist = CoolUtil.coolTextFile(Paths.txt('freeplaySonglist'));
 		for (i in 0...initSonglist.length)
 		{
@@ -82,70 +82,70 @@ class FreeplayState extends MusicBeatState
 				addSong(songArray[0], 0, songArray[1], Std.parseInt(songArray[2]));
 			}
 		}*/
-
+		
 		bg = new FlxSprite().loadGraphic(Paths.image('menuBGBlue'));
 		bg.antialiasing = false;
 		add(bg);
 		bg.screenCenter();
-
-		sideBars = new FlxBackdrop(Paths.image('sidebars'), 0.5, 0.5, false, true);
+		
+		sideBars = new FlxBackdrop(Paths.image('sidebars'), 0.5, 0.5);
 		sideBars.antialiasing = false;
 		sideBars.velocity.set(0, 1200);
 		add(sideBars);
 		sideBars.screenCenter();
-
+		
 		grpSongs = new FlxTypedGroup<FreeplayItem>();
 		add(grpSongs);
-
+		
 		grptxtsongs = new FlxTypedGroup<FlxText>();
 		add(grptxtsongs);
-
+		
 		for (i in 0...boxArray.length)
 		{
-				var box:FreeplayItem = new FreeplayItem(-20, 200, 'FreeBox');
-				box.ID = i;
+				var box:FreeplayItem = new FreeplayItem(-100, 200, 'FreeBox');
+				//box.ID = i;
 				box.y += ((box.width - 450) + 40);
-				box.setGraphicSize(Std.int(box.width * 0.5));
+				box.setGraphicSize(Std.int(box.width * 0.89));
 				grpSongs.add(box);
-
-				var char:FreeplayItem = new FreeplayItem(-20, 200, 'fpstuff/' + boxArray[i]);
-				char.ID = i;
+		
+				var char:FreeplayItem = new FreeplayItem(-100, 200, 'fpstuff/' + boxArray[i].toLowerCase());
+				//char.ID = i;
 				char.y += ((char.width - 450) + 40);
-				char.setGraphicSize(Std.int(char.width * 0.5));
+				char.setGraphicSize(Std.int(char.width * 0.89));
 				grpSongs.add(char);
 		}
 			//changeSelection();
-
+		
 		var swag:Alphabet = new Alphabet(1, 0, "swag");
-
+		
 			var songsText:FlxText = new FlxText(0, 0, 0, "", 32);
 			songsText.setFormat(Paths.font('sonic-cd-menu-font.ttf'), 32);
 			songsText.text = songArray[curSelected];
 			songsText.setPosition(900, 500);
 			add(songsText);
-
+		
 		// JUST DOIN THIS SHIT FOR TESTING!!!
 		/* 
 			var md:String = Markdown.markdownToHtml(Assets.getText('CHANGELOG.md'));
-
+		
 			var texFel:TextField = new TextField();
 			texFel.width = FlxG.width;
 			texFel.height = FlxG.height;
 			// texFel.
 			texFel.htmlText = md;
-
+		
 			FlxG.stage.addChild(texFel);
-
+		
 			// scoreText.textField.htmlText = md;
-
+		
 			trace(md);
 		 */
 		changeSelection();
-
+		
 		#if android
 		addVirtualPad(UP_DOWN, A_B);
 		#end
-
+		
 		super.create();
 	}
 
@@ -160,20 +160,20 @@ class FreeplayState extends MusicBeatState
 	private static var vocals:FlxSound = null;
 	var holdTime:Float = 0;
 	override function update(elapsed:Float)
-	{
+		{
 		if (FlxG.sound.music.volume < 0.7)
 		{
 			FlxG.sound.music.volume += 0.5 * FlxG.elapsed;
 		}
-
+		
 		var upP = controls.UI_UP_P;
 		var downP = controls.UI_DOWN_P;
 		var accepted = controls.ACCEPT;
 		var space = FlxG.keys.justPressed.SPACE #if android || _virtualpad.buttonX.justPressed #end;
 		var ctrl = FlxG.keys.justPressed.CONTROL #if android || _virtualpad.buttonC.justPressed #end;
-
+		
 		var shiftMult:Int = 1;
-
+		
 		if(songArray.length > 1)
 		{
 			if (upP)
@@ -186,13 +186,13 @@ class FreeplayState extends MusicBeatState
 				changeSelection(shiftMult);
 				holdTime = 0;
 			}
-
+		
 			if(controls.UI_DOWN || controls.UI_UP)
 			{
 				var checkLastHold:Int = Math.floor((holdTime - 0.5) * 10);
 				holdTime += elapsed;
 				var checkNewHold:Int = Math.floor((holdTime - 0.5) * 10);
-
+		
 				if(holdTime > 0.5 && checkNewHold - checkLastHold > 0)
 				{
 					changeSelection((checkNewHold - checkLastHold) * (controls.UI_UP ? -shiftMult : shiftMult));
@@ -200,7 +200,7 @@ class FreeplayState extends MusicBeatState
 				}
 			}
 		}
-
+		
 		if (controls.BACK)
 		{
 			persistentUpdate = false;
@@ -210,67 +210,48 @@ class FreeplayState extends MusicBeatState
 			else if (accepted)
 			{
 				persistentUpdate = false;
-				//ok:String = boxArray[1][curSelected];
+				ok:String = songArray[curSelected].toLowerCase();
 				FlxG.sound.play(Paths.sound('confirmMenu'));
-				switch(curSelected)
-				{
-					case 0:
-						PlayState.SONG = Song.loadFromJson('endless', 'endless-hard');
-					case 1:
-						PlayState.SONG = Song.loadFromJson('endless-og', 'endless-og-hard');
-					case 2:
-						PlayState.SONG = Song.loadFromJson('cycles', 'cycles-hard');
-					case 3:
-						PlayState.SONG = Song.loadFromJson('milk', 'milk-hard');
-					case 4:
-						PlayState.SONG = Song.loadFromJson('sunshine', 'sunshine-hard');
-					case 5:
-						PlayState.SONG = Song.loadFromJson('soulless', 'soulless-hard');
-					case 6:
-						PlayState.SONG = Song.loadFromJson('prey', 'Prey-hard');
-					case 7:
-						PlayState.SONG = Song.loadFromJson('fight-or-flight', 'fight-or-flight-hard');
-
+				PlayState.SONG = Song.loadFromJson(ok, ok + '-hard');
+				
+					
+					/*#if MODS_ALLOWED
+					if(!sys.FileSystem.exists(Paths.modsJson(songLowercase + '/' + poop)) && !sys.FileSystem.exists(Paths.json(songLowercase + '/' + poop))) {
+					#else
+					if(!OpenFlAssets.exists(Paths.json(songLowercase + '/' + poop))) {
+					#end
+						poop = songLowercase;
+						curDifficulty = 1;
+						trace('Couldnt find file');
+					}*/
+					//trace(poop);
+		
+					
+					if (FlxG.keys.pressed.SHIFT #if android || _virtualpad.buttonZ.pressed #end){
+						LoadingState.loadAndSwitchState(new ChartingState());
+					}else{
+						LoadingState.loadAndSwitchState(new PlayState());
+					}
+		
+					FlxG.sound.music.volume = 0;
 				}
-				
-				
-				/*#if MODS_ALLOWED
-				if(!sys.FileSystem.exists(Paths.modsJson(songLowercase + '/' + poop)) && !sys.FileSystem.exists(Paths.json(songLowercase + '/' + poop))) {
-				#else
-				if(!OpenFlAssets.exists(Paths.json(songLowercase + '/' + poop))) {
-				#end
-					poop = songLowercase;
-					curDifficulty = 1;
-					trace('Couldnt find file');
-				}*/
-				//trace(poop);
-	
-				
-				if (FlxG.keys.pressed.SHIFT #if android || _virtualpad.buttonZ.pressed #end){
-					LoadingState.loadAndSwitchState(new ChartingState());
-				}else{
-					LoadingState.loadAndSwitchState(new PlayState());
-				}
-	
-				FlxG.sound.music.volume = 0;
-			}
-		else if(space)
-		{
-			if(instPlaying != curSelected)
+			else if(space)
 			{
-				#if PRELOAD_ALL
-				destroyFreeplayVocals();
-				FlxG.sound.music.volume = 0;
-				if (PlayState.SONG.needsVoices)
-					vocals = new FlxSound().loadEmbedded(Paths.sound('bruh'));
-				else
-					vocals = new FlxSound();
-
-				FlxG.sound.list.add(vocals);
-				vocals.play();
-				vocals.persist = true;
-				vocals.looped = true;
-				vocals.volume = 0.7;
+				if(instPlaying != curSelected)
+				{
+					#if PRELOAD_ALL
+					destroyFreeplayVocals();
+					FlxG.sound.music.volume = 0;
+					if (PlayState.SONG.needsVoices)
+						vocals = new FlxSound().loadEmbedded(Paths.sound('bruh'));
+					else
+						vocals = new FlxSound();
+		
+					FlxG.sound.list.add(vocals);
+					vocals.play();
+					vocals.persist = true;
+					vocals.looped = true;
+					vocals.volume = 0.7;
 				instPlaying = curSelected;
 				#end
 			}
@@ -300,7 +281,7 @@ class FreeplayState extends MusicBeatState
 
 		for (item in grpSongs.members)
 		{
-			FlxTween.tween(item, {"scale.x": 0.85,"scale.y": 0.85}, 0.25);
+			FlxTween.tween(item, {"scale.x": 0.75,"scale.y": 0.75}, 0.35);
 			item.alpha = 0.45;
 			// item.setGraphicSize(Std.int(item.width * 0.8));
 
@@ -308,28 +289,8 @@ class FreeplayState extends MusicBeatState
 			{
 				item.alpha = 1;
 				//songslol = songs;
-				FlxTween.tween(item, {"scale.x": 1,"scale.y": 1}, 0.25);
+				FlxTween.tween(item, {"scale.x": 1,"scale.y": 1}, 0.35);
 				// item.setGraphicSize(Std.int(item.width));
-			}
-
-			if (change == 1) 
-			{
-				cdman = false;
-				FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
-				FlxTween.tween(item ,{x: item.x + 1080}, 0.2, {ease: FlxEase.expoOut, onComplete: function(sus:FlxTween)
-					{
-						cdman = true;
-					}
-				});
-			}
-			if (change == -1) 
-			{
-				cdman = false;
-				FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
-				FlxTween.tween(item, {y: item.y - 1080}, 0.2, {ease: FlxEase.expoOut, onComplete: function(sus:FlxTween)
-				{
-					cdman = true;
-				}});
 			}
 		}
 	}
