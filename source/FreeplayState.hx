6db725692217361637e904f090a907ad9b9adc4e
Aly-Ant <90076069+Aly-Ant0@users.noreply.github.com>
2022-08-12 01:57:59 -0300

    aaaaaaaaaaaa

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -14,6 +14,7 @@ import flixel.group.FlxSpriteGroup.FlxTypedSpriteGroup;
 import flixel.math.FlxMath;
 import flixel.text.FlxText;
 import flixel.util.FlxColor;
+import flixel.util.FlxTimer;
 import flixel.addons.display.FlxBackdrop;
 import flixel.tweens.FlxTween;
 import flixel.tweens.FlxEase;
